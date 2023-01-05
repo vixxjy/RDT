0066928336efea889fe8f2782e7ecbc298753d7c
vixxjy <ajorvictor48@gmail.com>
2023-01-05 21:34:26 +0100

    Error and exception handling.

---
+++ src/main/java/com/example/demo/exceptions/APIException.java
@@ -0,0 +1,9 @@
+package com.example.demo.exceptions;
+
+public class APIException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public APIException(String message) {
+        super(message);
+    }
+}
